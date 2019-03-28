e41c9bdad43eaf03f7fba9572c42675ca00f32d3
Jason Jackson <20131836@sun.ac.za>
2019-03-28 15:55:16 +0200

    Thinking about how the vis format works to get declination

--- Pipeline.py
+++ Pipeline.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     # ut.plot_array(layout)
     b12 = layout[1] - layout[2]
     L,f = TR.get_latitude_and_frequency()
-    ut.plot_baseline(b12, L, f, layout[0], layout[1])
+    # ut.plot_baseline(b12, L, f, layout[0], layout[1])
     print(TR.get_visibilities())
--- Utilities.py
+++ Utilities.py
@@ -16,7 +16,21 @@ def plot_baseline(b_ENU, L, f, ant1, ant2):
                 D * (math.sin(L)*math.sin(E) + math.cos(L) * math.cos(E)*math.cos(A))])
     c = scipy.constants.c                                        # Speed of light
     lam = c/f
-    
+    # dec = ?
+    time_steps = 600
+    h = np.linspace(-12,12,num=time_steps)*np.pi/12
+    X = B[0]
+    Y = B[1]
+    Z = B[2]
+    u = lam**(-1)*(np.sin(h)*X+np.cos(h)*Y)
+    v = lam**(-1)*(-np.sin(dec)*np.cos(h)*X+np.sin(dec)*np.sin(h)*Y+np.cos(dec)*Z)
+    w = lam**(-1)*(np.cos(dec)*np.cos(h)*X-np.cos(dec)*np.sin(h)*Y+np.sin(dec)*Z)
+    # plotBL.UV(u,v,w)
+    a=np.sqrt(X**2+Y**2)/lam # major axis
+    b=a*np.sin(dec)              # minor axis
+    v0=(Z/lam)*np.cos(dec)  # center of ellipse
+    plotBL.UVellipse(u,v,w,a,b,v0)
+
 def plot_array(antennas):
     plt.scatter(antennas[:,0], antennas[:,1])
     plt.grid(True)
